cbed06bbbd000fedcb043fedfcdb8ba049a0228c
GN <guillaume.nadal13@gmail.com>
2024-07-26 15:45:31 -0400

    add detail sample end point

--- redux/features/sampleApiSlice.ts
+++ redux/features/sampleApiSlice.ts
@@ -3,11 +3,19 @@ import { apiSlice } from '../services/apiSlice';
 const sampleApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
         fetchSampleModelByName: builder.query({
-            query: (name) => `samples/name/${name}/`,
+            query: (name) => `samples/description/${name}/`,
         }),
+        UserMachine: builder.query({
+            query: () => `samples/user/`,
+        }),
+        DetailSample:builder.query({
+            query: (name) => `samples/detail/${name}/`,
+        })
     }),
 });
 
 export const {
     useFetchSampleModelByNameQuery,
+    useUserMachineQuery,
+    useDetailSampleQuery
 } = sampleApiSlice;
